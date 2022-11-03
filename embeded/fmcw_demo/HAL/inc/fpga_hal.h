6979cd786fa7361556db6bf08a9b2bd2e4a3b9b4
cccc <cuiwei_cv@163.com>
2022-11-03 17:22:24 +0800

    fmcw_demo: add spi3

--- embeded/fmcw_demo/HAL/inc/fpga_hal.h
+++ embeded/fmcw_demo/HAL/inc/fpga_hal.h
@@ -2,5 +2,6 @@
 #define _DC2CAE9E_3D36_42FA_9F25_27A17C630EC1_
 
 void fpga_init(void);
+void spi3_init(void);
 
 #endif//_DC2CAE9E_3D36_42FA_9F25_27A17C630EC1_
--- embeded/fmcw_demo/HAL/src/fpga_hal.c
+++ embeded/fmcw_demo/HAL/src/fpga_hal.c
@@ -3,6 +3,46 @@
 
 void fpga_init(void)
 {
-    //
+    spi3_init();
 }
 
+void spi3_init(void)
+{
+    GPIO_PinAFConfig(GPIOA, GPIO_PinSource15, GPIO_AF5_SPI3);
+    GPIO_PinAFConfig(GPIOC, GPIO_PinSource10, GPIO_AF5_SPI3);
+    GPIO_PinAFConfig(GPIOC, GPIO_PinSource11, GPIO_AF5_SPI3);
+    GPIO_PinAFConfig(GPIOC, GPIO_PinSource12, GPIO_AF5_SPI3);
+
+    GPIO_InitTypeDef GPIO_InitStructure;
+
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_DOWN;
+
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_15;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_10;
+    GPIO_Init(GPIOC, &GPIO_InitStructure);
+
+    GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_11;
+    GPIO_Init(GPIOC, &GPIO_InitStructure);
+
+    GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_12;
+    GPIO_Init(GPIOC, &GPIO_InitStructure);
+
+    SPI_InitTypeDef  SPI_InitStructure;
+
+    SPI_InitStructure.SPI_Mode = SPI_Mode_Master;
+    SPI_InitStructure.SPI_Direction = SPI_Direction_1Line_Tx;
+    SPI_InitStructure.SPI_DataSize = SPI_DataSize_8b;
+    SPI_InitStructure.SPI_CPOL = SPI_CPOL_Low;
+    SPI_InitStructure.SPI_CPHA = SPI_CPHA_1Edge;
+    SPI_InitStructure.SPI_NSS = SPI_NSS_Hard;
+    SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_256;
+    SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_MSB;
+    SPI_Init(SPI3, &SPI_InitStructure);
+
+    SPI_Cmd(SPI3, ENABLE);
+}
